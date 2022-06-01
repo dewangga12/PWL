561481ff7c3911bf8f6576f70eb115758290c66b
dewangga12 <dewanggajr30@gmail.com>
2022-06-01 19:31:40 +0700

    JS10[Prak2] : Add edit article feature

--- jobsheet 6/authentikasi_id/app/Http/Controllers/ArticleController.php
+++ jobsheet 6/authentikasi_id/app/Http/Controllers/ArticleController.php
@@ -68,10 +68,12 @@ class ArticleController extends Controller
      *
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
-     */
-    public function edit(Article $article)
+     */ 
+    public function edit($id)
     {
-        //
+        $article = Article::find($id);
+
+        return view('articles.edit', ['article' => $article]);
     }
 
     /**
@@ -81,9 +83,22 @@ class ArticleController extends Controller
      * @param  \App\Models\Article  $article
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Article $article)
+    public function update(Request $request, $id)
     {
-        //
+        $article = Article::find($id);
+
+        $article->title = $request->title;
+        $article->content = $request->content;
+
+        if ($article->featured_image && file_exists(storage_path('app/public/' . $article->featured_image))) {
+            Storage::delete('public/' . $article->featured_image);
+        }
+        $image_name = $request->file('image')->store('images', 'public');
+        $article->featured_image = $image_name;
+
+        $article->save();
+        return redirect()->route('articles.index')
+            ->with('success', 'Artikel Behasil Diupdate');
     }
 
     /**
